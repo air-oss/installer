fb641c57d0ccf89eeb09481ee67fe6fa9b92bb6b
Oskar Nizioł <oskarniziol@gmail.com>
2022-06-05 20:40:04 +0200

    Ok

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="h-full">
-
+    <div className="content-center text-center text-white bg-neutral-800 w-3/4">
+      Test
     </div>
   )
 }
