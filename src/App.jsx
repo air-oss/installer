5b07fd1db2225042f7471a6109b6031d9bbfe52a
Oskar Nizioł <oskarniziol@gmail.com>
2022-06-06 15:56:00 +0200

    Adding css fonts

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,9 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="content-center text-center text-white bg-neutral-800 w-3/4">
-      Test
+    <div className="content-center text-center text-white bg-neutral-800 w-3/4 h-2/4 rounded-md">
+      <h1 className="text-4xl">Welcome</h1>
+      <h1 className="text-1xl text-slate-500">Welcome to Air Os Installer, now select your launguage</h1>
     </div>
   )
 }
